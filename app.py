056d9d6d1c4517e50ca62d333ed41f20bc8603bd
gokusya-star <yu39haru911@gmail.com>
2025-08-17 03:36:12 +0900

    third commit

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from PIL import Image
 
 #文字
 st.title("しりとり")
+
+image = Image.open('タイトルなし.png')
+st.image(image, width=200)
+"""
 n = random.randint(1,100)
 st.caption(n)
 st.subheader("HEY BRO")
@@ -24,21 +28,21 @@ st.image(image, width=200)
 #video_file = open('無題の動画 ‐ Clipchampで作成 (13).mp4', 'rb')
 #video_bytes = video_file.read()
 #st.video(video_bytes)
+# """
 
 with st.form(key='form'):
     name = st.text_input('名前')
 
-    slct = st.radio('好き？',('好き','普通','嫌い'))
+    slct = st.radio('ごくしゃのこと好き？',('好き','普通','嫌い'))
     #slct = st.selectbox('好き？',('好き','普通','嫌い'))
-
-    prime = st.multiselect('素数は？',('1','2','3','4','5'))
 
     submit_btn = st.form_submit_button('送信')
     cancel_btn = st.form_submit_button('キャンセル')
 
     if submit_btn:
-        st.text(f'ようこそ{slct}な{", ".join(prime)}の{name}さん')
-        print(name,slct,", ".join(prime))
+        st.text(f'あなた{name}っていうのね！')
+        st.text(f'となりの{name}っ{name}っ♪')
+        print(name,slct)
     #print(f'submit_btn: {submit_btn}')
     #print(f'cancel_btn: {cancel_btn}')
 
