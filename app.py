e03eff6292df084e349cca9bb612a8659379f826
gokusya-star <yu39haru911@gmail.com>
2025-08-17 03:37:24 +0900

    third commit

--- app.py
+++ app.py
@@ -7,34 +7,11 @@ st.title("しりとり")
 
 image = Image.open('タイトルなし.png')
 st.image(image, width=200)
-"""
-n = random.randint(1,100)
-st.caption(n)
-st.subheader("HEY BRO")
-st.text("WHATS UP?")
-
-#コード
-code = '''
-import streamlit as st
-
-st.title("しりとり")
-'''
-st.code(code, language='python')
-
-#画像
-image = Image.open('タイトルなし.png')
-st.image(image, width=200)
-#動画
-#video_file = open('無題の動画 ‐ Clipchampで作成 (13).mp4', 'rb')
-#video_bytes = video_file.read()
-#st.video(video_bytes)
-# """
 
 with st.form(key='form'):
     name = st.text_input('名前')
 
     slct = st.radio('ごくしゃのこと好き？',('好き','普通','嫌い'))
-    #slct = st.selectbox('好き？',('好き','普通','嫌い'))
 
     submit_btn = st.form_submit_button('送信')
     cancel_btn = st.form_submit_button('キャンセル')
@@ -43,8 +20,6 @@ with st.form(key='form'):
         st.text(f'あなた{name}っていうのね！')
         st.text(f'となりの{name}っ{name}っ♪')
         print(name,slct)
-    #print(f'submit_btn: {submit_btn}')
-    #print(f'cancel_btn: {cancel_btn}')
 
 # セッション初期化
 if "used_hiragana" not in st.session_state:
